6b405977b0ee52504a35235e1edd8a7f6334448e
yuhuan520 <2601644091@qq.com>
2020-07-29 22:29:52 +0800

    跟新1

---
+++ src/main/java/com/java/springboot/controller/ModelController.java
@@ -0,0 +1,14 @@
+package com.java.springboot.controller;
+
+
+/**
+ * @program: springBoot_emp
+ * @description: 测试
+ * @author: Mr.Yu
+ * @create: 2020-07-29 22:27
+ **/
+public class ModelController {
+    public static void main(String[] args) {
+        System.out.println("更新1");
+    }
+}
